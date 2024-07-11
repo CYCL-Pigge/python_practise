ea22b4960cfbd330114f76ad42d12d065097a24d
CYCL-Pigge <167639308+CYCL-Pigge@users.noreply.github.com>
2024-07-11 13:53:21 +0800

    Update PS5购买条件符合性测试.py

--- PS5购买条件符合性测试.py
+++ PS5购买条件符合性测试.py
@@ -1,9 +1,11 @@
-girlfirend_manner = input("你女朋友是否支持买PS5：")
-wollet = input("你钱包是否宽裕：")
-user_manner = input('你是否真的喜欢主机游戏：')
-if girlfirend_manner == '是' and wollet == '是' and user_manner == '是':
-    print('那就买它！')
-elif girlfirend_manner == '否' and wollet == '是' and user_manner == '是':
-    print('女朋友可能会打你，谨慎考虑')
+girlfriend_manner = input("你女朋友是否支持买PS5（是/否）：")
+wallet = input("你钱包是否宽裕（是/否）：")
+user_manner = input('你是否真的喜欢主机游戏（是/否）：')
+
+if user_manner == '是' and wallet == '是':
+    if girlfriend_manner == '是':
+        print('那就买它！')
+    else:
+        print('女朋友可能会打你，谨慎考虑')
 else:
     print('没必要，放弃吧')
